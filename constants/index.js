a132367a080eee13ce69d709f27e5203b61e6ec2
ivan farid <ivan99edlp@gmail.com>
2023-08-24 18:24:10 -0300

    added project and better descriptions on each one

--- constants/index.js
+++ constants/index.js
@@ -166,20 +166,20 @@ export const allProjectsEn = [
   {
     title: 'El Producto',
     link: 'https://futapp-steel.vercel.app/competiciones',
-    description: 'Soccer app to see live scores and standings with a forum section made with React and Material UI.',
+    description: 'Soccer app to see live scores and standings from any league with a forum section where users can post made with React, Material UI and Sequelize.',
     imgUrl: '/futApp.png',
   },
   {
     title: 'Anime DB',
     link: 'https://db-anime.vercel.app/',
-    description: "App to search for any Anime and see all it's information made with React and Material UI.",
+    description: "App to search any Anime and see all it's information made with React and Material UI.",
     imgUrl: '/animedb.png',
   }
   ,
   {
     title: 'XClone App',
     link: 'https://github.com/IvoFarid/finalSX',
-    description: "Twitter clone, made with Symfony, SymfonyUX initiative, Tailwind and Hotwire Turbo.",
+    description: "Twitter clone, made with Symfony, SymfonyUX initiative, Tailwind and Hotwire Turbo. Has the basics of tweeting, respond each other, retweet, like, etc. Users can also follow each other, and more!",
     imgUrl: '/symfony1.png',
   }
   ,
@@ -189,6 +189,12 @@ export const allProjectsEn = [
   //   description: "App that allows users publish campgrounds on a cluster map. Made with HTML, EJS and Bootstrap.",
   //   imgUrl: '/campgrounds.png',
   // },
+  {
+    title: 'UMAD | 2023',
+    link: 'https://github.com/farmMLP/Farm',
+    description: 'Official Medical App for the UMAD in La Plata. Has management of products, stocks, orders, health centers, admins, and so much more. Made mostly with Symfony and Bootstrap.',
+    imgUrl: '/UMAD.png'
+  },
   {
     title: 'Chat App',
     link: 'https://chatapp-mi4r.onrender.com/',
@@ -207,20 +213,20 @@ export const allProjectsEs = [
   {
     title: 'El Producto',
     link: 'https://futapp-steel.vercel.app/competiciones',
-    description: 'App de fútbol para ver resultados, posiciones en vivo y con un foro incluido hecho con React y Material UI.',
+    description: 'App de fútbol que permite ver resultados, posiciones en vivo de cualquier liga y con un foro de usuarios funcional, hecho con React, Material UI y Sequelize.',
     imgUrl: '/futApp.png',
   },
   {
     title: 'Anime DB',
     link: 'https://db-anime.vercel.app/',
-    description: "App para buscar cualquier Animé y ver toda su información, realizada con React y Material UI.",
+    description: "App que permite buscar cualquier Animé y ver toda su información, realizada con React y MaterialUI.",
     imgUrl: '/animedb.png',
   }
   ,
   {
     title: 'XClone App',
     link: 'https://github.com/IvoFarid/finalSX',
-    description: "Twitter clon, hecho con Symfony, SymfonyUX, Tailwind y Hotwire Turbo.",
+    description: "Twitter clon, hecho con Symfony, SymfonyUX, Tailwind y Hotwire Turbo. Posee las características básicas de publicación, acciones sobre los tweets, seguimiento de usuarios y estadísticas.",
     imgUrl: '/symfony1.png',
   }
   ,
@@ -238,9 +244,15 @@ export const allProjectsEs = [
   //   imgUrl: '/campgrounds.png',
   // },
   {
+    title: 'UMAD | 2023',
+    link: 'https://github.com/farmMLP/Farm',
+    description: 'Página para el centro UMAD en La Plata. Contiene interfaz de admin, manejo de productos, stocks, pedidos solicitados, autorización de los mismos, centros de salud y mucho más.',
+    imgUrl: '/UMAD.png'
+  },
+  {
     title: 'Chat App',
     link: 'https://chatapp-mi4r.onrender.com/',
-    description: "App que permite chatear en tiempo real, hecho con HTML, CSS, JS y Socket IO.",
+    description: "Página que permite chatear en tiempo real gracias a Socket IO, realizado también con HTML, CSS y JS.",
     imgUrl: '/ChatDemo.png',  
   },
   // {
